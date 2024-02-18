cceb5e49acf9bac75a4ea2f2075702e375a1143c
alif iqbal <alifcodes101@gmail.com>
2024-02-18 23:48:02 +0600

    added more functionality

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
 import tkinter as tk
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+
+def open_file(window, text_edit):
+    filepath = askopenfilename(filetypes=[("Text Files", "*.txt")])
+
+    if not filepath:
+        return
+
+    text_edit.delete(1.0,tk.END)
+    with open(filepath, "r") as f:
+        content = f.read()
+        text_edit.insert(tk.END, content)
+    window.title(f"Open File: {filepath}")
+
+def save_file(window, text_edit):
+    filepath = asksaveasfilename(filetypes=[("Text Files", "*.txt")])
+
+    if not filepath:
+        return
+
+    with open(filepath, "w") as f:
+        content = text_edit.get(1.0, tk.END)
+        f.write(content)
+    window.title(f"Open File: {filepath}")
+    
+
 
 def main():
     window = tk.Tk()
@@ -11,13 +37,12 @@ def main():
     text_edit.grid(row=0, column=1)
 
     frame = tk.Frame(window, relief=tk.RAISED, bd=2)
-    save_button = tk.Button(frame, text="save")
-    open_button = tk.Button(frame, text="open")
+    save_button = tk.Button(frame, text="save", command=lambda: save_file(window, text_edit))
+    open_button = tk.Button(frame, text="open", command=lambda: open_file(window, text_edit))
 
     save_button.grid(row=0, column=0, padx=5, pady=5, sticky="ew")
     open_button.grid(row=1, column=0, padx=5, sticky="ew")
     frame.grid(row=0, column=0, sticky="ns")
-
 
     window.mainloop()
 
