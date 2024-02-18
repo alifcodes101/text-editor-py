b852a962b1c12a44fb4ba277a8ab1a94706b0ebe
alif iqbal <alifcodes101@gmail.com>
2024-02-18 23:10:09 +0600

    wrote some more code

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-import tkinter as tkinter
+import tkinter as tk
 
 def main():
     window = tk.Tk()
     window.title("Text Editor")
+    window.rowconfigure(0, minsize=400)
+    window.columnconfigure(1, minsize=500)
+
+
+    text_edit = tk.Text(window, font="Helvetica 18")
+    text_edit.grid(row=0, column=1)
+
+    frame = tk.Frame(window, relief=tk.RAISED, bd=2)
+    save_button = tk.Button(frame, text="save")
+    open_button = tk.Button(frame, text="open")
+
+    save_button.grid(row=0, column=0, padx=5, pady=5, sticky="ew")
+    open_button.grid(row=1, column=0, padx=5, sticky="ew")
+    frame.grid(row=0, column=0, sticky="ns")
+
 
     window.mainloop()
 
